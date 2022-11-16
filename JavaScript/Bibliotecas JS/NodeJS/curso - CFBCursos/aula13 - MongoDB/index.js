65bc01a28b0a75f905bce50ba4c1f30e5cb73500
NatanSan2002 <natansantosgomes@gmail.com>
2022-11-16 14:39:50 -0300

    Curso NodeJS -  MongoDB Insert,Join

--- JavaScript/Bibliotecas JS/NodeJS/curso - CFBCursos/aula13 - MongoDB/index.js
+++ JavaScript/Bibliotecas JS/NodeJS/curso - CFBCursos/aula13 - MongoDB/index.js
@@ -11,22 +11,49 @@ const URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB
 mongo.connect(URL, (error,banco) =>{
 if(error){throw error};
 
-const dbo = banco.db("Nome_Banco");
+const dbo = banco.db("Nome_Banco(Treinamento)");
 
-const obj = [
-    {curso: "Curso de JS",autor:"Canal CFB Cursos"},
-    {curso: "Curso de JS",autor:"Canal CFB Cursos"},
-    {curso: "Curso de JS",autor:"Canal CFB Cursos"}
+const obj_cursos = [
+    {idcurso:01, curso:"C++", autor:"CFB Cursos"},
+    {idcurso:02, curso:"HTML", autor:"CFB Cursos"},
+    {idcurso:03, curso:"CSS", autor:"CFB Cursos"},
+    {idcurso:04, curso:"JavaScript", autor:"CFB Cursos"},
+    {idcurso:05, curso:"PHP", autor:"CFB Cursos"},
+    {idcurso:06, curso:"NodeJS", autor:"CFB Cursos"},
+    {idcurso:07, curso:"React", autor:"CFB Cursos"},
+    {idcurso:08, curso:"React-Native", autor:"CFB Cursos"},
+    {idcurso:09, curso:"Arduino", autor:"CFB Cursos"},
+    {idcurso:10, curso:"Photoshop", autor:"CFB Cursos"},
+    {idcurso:11, curso:"AppInventor", autor:"CFB Cursos"},
+    {idcurso:12, curso:"Flash", autor:"CFB Cursos"},
+    {idcurso:13, curso:"Unity", autor:"CFB Cursos"},
+    {idcurso:14, curso:"C#", autor:"CFB Cursos"},
+    {idcurso:15, curso:"Java", autor:"CFB Cursos"},
+    {idcurso:16, curso:"QT Creator", autor:"CFB Cursos"},
+    {idcurso:17, curso:"SQL", autor:"CFB Cursos"},
+    {idcurso:18, curso:"Python", autor:"CFB Cursos"},
 ];
 
 const colecao = "Cursos";
 
-/*dbo.collection(colecao).insertMany(obj, async (error,result) =>{  //Inserindo Dados
-if(error){throw error};
+let obj_aulas = [];
+
+for(i=0,y=20;i<18;++i,y+=20){    // looping para inserir objetos porque tava com preguiça :p
+obj_aulas[i] = {idcurso:i+1,aulas:y}
+}
+
+
+const colecao2 = "Aulas";
+
+
+
+
+/*dbo.collection(colecao2).insertMany(obj_aulas, async (error,result) =>{  //Inserindo Dados
+if(error) throw error;
 
 await console.log(result.insertedCount+" Novo(s) Curso(s) Inserido(s)");
-banco.close();/*
-} );
+banco.close();
+} );/*
 
 /*dbo.collection(colecao).find({},{projection:{_id:0}}).toArray((error,result) =>{ //Busca de Dados
     if(error){throw error};
@@ -73,16 +100,47 @@ banco.close();/*
     
     } ); 
 
-     query = {};  */
+     query = {};  
 
 
-    dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((error,result) =>{ 
+    dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((error,result) =>{  // Buscando Dados na coleção
     if(error){throw error};
     
     console.log(result);
     banco.close();
-    } ); 
+    } ); */
 
+
+
+    /* let query = {curso: 'Curso de NodeJS'};
+
+    let new_value = {$set:{curso: 'Curso de Node', autor: "update_test"}}  
+
+
+
+    dbo.collection(colecao).updateMany(query,new_value,async(error,result) => {  // Atualizando Dados Na Coleção
+     if(error){throw error};
+     
+    
+     await console.log(result.modifiedCount+" dados(s) atualizados(s)");
+    //banco.close();
+    } ); */
+
+
+
+
+
+   /* query = {curso:"Curso de Node"};       // Busca Dados Coleção (Testes Recorrente da aplicação)
+
+
+    dbo.collection(colecao).find(query,{projection:{_id:0}}).limit(2).toArray((error,result) =>{  
+    if(error){throw error};
+    
+    console.log(result);
+    banco.close();
+    } ); */
 
 });
+
+
 
